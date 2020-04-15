8c4f9f40dd3ac2ced05123f62ecd86d2d4e9aed3
luxiangqiang <1040500646@qq.com>
2020-04-15 23:15:22 +0800

    打卡第1天 求两数之和

---
+++ 1、求两数之和.js
@@ -0,0 +1,63 @@
+/**
+ * 解法一:暴力破解
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  // 判断数组为空的情况
+  if (nums == null || nums.length == 1) {
+    return [];
+  }
+  for (let i = 0; i < nums.length; i++) {
+    let item = nums[i];
+    if (nums.indexOf(target - item, i + 1) !== -1) {
+      console.log(nums.indexOf(target - item, i + 1));
+      return [i, nums.indexOf(target - item, i + 1)];
+    }
+  }
+  return [];
+};
+
+/**
+ * 解法二:两遍哈希表法
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum2 = function(nums, target) {
+  // 将值存储到哈希表中
+  let map = new Map();
+  // 存储
+  nums.forEach((item,index) => {
+    map.set(item, index);
+  });
+  // 判断
+  for (let i = 0; i < nums.length; i++) {
+    let item = nums[i];
+    console.log(map.get(target - item))
+    if (map.has(target - item) &&  map.get(target - item) !== i) {
+      return [i, map.get(target - item)];
+    }
+  }
+  return [];
+};
+
+var twoSum3 = function(nums, target) {
+  // 将值存储到哈希表中
+  let map = new Map();
+  // 存储
+  for (let i = 0; i < nums.length; i++) {
+    let item = nums[i];
+    if (map.has(target - item) &&  map.get(target - item) !== i) {
+      return [i, map.get(target - item)];
+    }
+    map.set(item, i);
+  }
+  return [];
+};
+
+console.log(twoSum2([2,7,11,15],9))
+
+
